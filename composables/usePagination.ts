57c02463efa41cba9acdaa2630c967efb0c18480
Jenya Golovnov <93ave.blues@gmail.com>
2024-02-27 18:40:23 +0100

    vuetify / basic markup / project fetch logic

--- composables/usePagination.ts
+++ composables/usePagination.ts
@@ -1,1 +1,12 @@
-export const usePagination = (queryKey: string) => {};
+interface ProjectRestructured {
+    name: string;
+    startDate: string;
+    endDate: string;
+    releaseStatus: string;
+    statusDescription: string;
+    website: string;
+  }
+
+export const usePagination = (projects: ComputedRef<ProjectRestructured[]>, page = 10) => {
+    return projects.value.slice();
+};
--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,72 @@
   <div v-if="error">
     <p>{{ error }}</p>
   </div>
-  <div class="main-container"></div>
+  <v-container class="bg-surface-variant" fluid>
+    <v-row>
+      <v-col>
+        <v-date-picker elevation="24" />
+      </v-col>
+      <v-col>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts">
 import { QUERY_KEYS } from '~/constants/index';
 import { API_URL } from '~/constants/api';
 
+interface ProjectsMetaData {
+  acronym: string;
+  lastUpdated: string;
+  projectId: number;
+  title: string;
+  website: string;
+}
+
+interface Project {
+  name: string;
+  startDate: string;
+  endDate: string;
+  releaseStatus: string;
+  statusDescription: string;
+  website: string;
+}
+
+const projectsList = ref<any[]>([]);
+
 const {
   data: projectsMetaData,
   pending,
   error,
   refresh
-} = await useAsyncData(
+} = await useAsyncData<{ projects: ProjectsMetaData[] }>(
   QUERY_KEYS.PROJECTS_METADATA,
-  () => $fetch(API_URL.PROJECTS, { query: { updatedSince: '2024-02-23', server: true } }),
+  () => $fetch(API_URL.PROJECTS,
+    { query: { updatedSince: '2024-02-23' } }
+  ),
 )
+
+watch(projectsMetaData, async newVal => {
+  try {
+    const projectDetailsPromises = newVal?.projects.map(project => (
+      $fetch(`${API_URL.PROJECTS}/${project.projectId}`)
+    ));
+    projectsList.value = await Promise.all(projectDetailsPromises ?? []);
+  } catch (error) {
+    console.error('Error fetching project details', error);
+  }
+}, { immediate: true });
+
+const projectsRestructured: globalThis.ComputedRef<Project[]> = computed(() => projectsList.value.map(item => ({
+    name: item.project.title,
+    startDate: item.project.startDateString,
+    endDate: item.project.endDateString,
+    releaseStatus: item.project.releaseStatusString,
+    statusDescription: item.project.statusDescription,
+    website: item.project.website,
+})));
+
+const paginatedProjects = computed(() => usePagination(projectsRestructured, 10));
+
 </script>
